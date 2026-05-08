9258621d004de167e42104e7801bf7e2ce858ced
MsShawnP <msshawnp@gmail.com>
2026-05-08 11:01:05 -0400

    scaffold: Vite + React app in web/ with Recharts dep

---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Cinderhaven Provisions &mdash; Cost of Short-Shipping Analysis</h1>
+    </main>
+  )
+}
+
+export default App
